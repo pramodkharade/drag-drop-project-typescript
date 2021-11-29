6a7f131a6d81da150f8e9317903b9b015f63e062
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2021-11-29 12:53:27 +0530

    created autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+// autobin Decorator
+function autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const ajDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return ajDescriptor;
+}
+//project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -28,12 +45,14 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
+
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log("Input Value:", this.titleInputElement.value);
   }
   private configure() {
-    this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
