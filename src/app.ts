0b9f169509adf56a06b8d81e6806b703565db49b
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2021-11-29 13:20:01 +0530

    gather data ,validate it, return either tuple or void

--- src/app.ts
+++ src/app.ts
@@ -45,11 +45,30 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
 
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log("Input Value:", this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log("Title:", title, " Desc:", desc, " People:", people);
+    }
   }
   private configure() {
     this.element.addEventListener("submit", this.submitHandler);
