18ca886559f0615a05a9dcc6cd920e8d9d6c3b4c
pramodkharade <kharade.pramod91@gmail.com>
2021-11-30 15:44:26 +0530

    webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
   },
   devtool: "inline-source-map",
   module: {
