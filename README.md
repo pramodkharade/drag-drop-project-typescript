9e125d593d4c6346a65f8120ab52a1e413573b49
pramodkharade <kharade.pramod91@gmail.com>
2021-11-30 16:15:02 +0530

    prod build webpack

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # drag-drop-project-typescript
 
-Nodejs with TypeScript project creation
+    Nodejs with TypeScript project creation
 
 # Setup NodeJS with TypeScript
 
@@ -10,5 +10,10 @@ npm install typescript --save-dev
 
 npx tsc --init --rootDir ./src --outDir ./dist --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
 
-#web Pack Setup
+# web Pack Setup
+
 npm i --save-dev webpack webpack-cli webpack-dev-server typescript ts-loader
+
+# to delete dist file before creating new prod build
+
+npm i clean-webpack-plugin --save-dev
---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+  },
+  devtool: "none",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
